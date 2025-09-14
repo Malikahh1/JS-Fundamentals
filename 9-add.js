8c3f1631c54ea9b4572b4f695f6960c72ece1212
Malikah <damilolaariyoz7@gmail.com>
2025-09-15 00:04:16 +0100

    9-add.js

---
+++ 9-add.js
@@ -0,0 +1,11 @@
+
+// Prints the addition of 2 integers
+
+function add(a, b) {
+  return a + b;
+}
+
+const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
+
+console.log(add(a, b));
