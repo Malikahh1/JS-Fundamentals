870d26622007e92cae17dbdc91dd821717eef6f2
Malikah <damilolaariyoz7@gmail.com>
2025-09-15 00:06:47 +0100

    8-square.js

---
+++ 8-square.js
@@ -0,0 +1,11 @@
+// Prints a square of size x using 'X'
+
+const size = Number(process.argv[2]);
+
+if (Number.isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
